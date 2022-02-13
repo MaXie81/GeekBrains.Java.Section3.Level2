4382e65982b3fbc96bda10ba0066cfce8dd23e97
KrakenUser <KrakenUser@yandex.ru>
2022-02-13 16:29:18 +0300

    Начало. Сервисы. Создан сервис для сущности Продукт(ProductService).

---
+++ src/main/java/webshop/services/ProductService.java
@@ -0,0 +1,25 @@
+package webshop.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import webshop.entities.Product;
+import webshop.repositories.ProductRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+}
