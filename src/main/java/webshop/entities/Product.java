e5745744c5b85886fb74024475e158c496eccfe5
KrakenUser <KrakenUser@yandex.ru>
2022-02-13 16:15:43 +0300

    Начало. Сущности. Создана сущность Продукт(Product).

---
+++ src/main/java/webshop/entities/Product.java
@@ -0,0 +1,26 @@
+package webshop.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+@Data
+@NoArgsConstructor
+public class Product {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "title")
+    private String title;
+    @Column(name = "prce")
+    private int price;
+}
