4faade53ffaee70a86f461213686fa0797a3cc83
KrakenUser <KrakenUser@yandex.ru>
2022-02-16 17:17:29 +0300

    Security. Контроллеры. Создан контроллер авторизации Пользователей приложения.

---
+++ src/main/java/webshop/controllers/AuthController.java
@@ -0,0 +1,36 @@
+package webshop.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import webshop.dtos.JwtRequest;
+import webshop.dtos.JwtResponse;
+import webshop.services.UserService;
+import webshop.utils.JwtTokenUtil;
+
+@RestController
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserService userService;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping("/auth")
+    public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
+        String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
+}
