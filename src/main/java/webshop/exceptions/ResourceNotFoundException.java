32ee728bdad12a0a95cde29d5f05a322545288bf
KrakenUser <KrakenUser@yandex.ru>
2022-02-17 20:41:56 +0300

    Exception. Создан класс ошибки(Exception), которая будет вызываться, если объект сущности не найден(ResourceNotFoundException).

---
+++ src/main/java/webshop/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package webshop.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
