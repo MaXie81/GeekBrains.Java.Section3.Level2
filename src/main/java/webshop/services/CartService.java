451126d9359bcd31f75911c1d0b12e5dd87005af
KrakenUser <KrakenUser@yandex.ru>
2022-02-18 12:02:56 +0300

    Корзина. Сервисы. Создан класс для объекта Корзина(CartService).

---
+++ src/main/java/webshop/services/CartService.java
@@ -0,0 +1,31 @@
+package webshop.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import webshop.dtos.Cart;
+import webshop.entities.Product;
+import webshop.exceptions.ResourceNotFoundException;
+
+import javax.annotation.PostConstruct;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final ProductService productService;
+    private Cart tempCart;
+
+    @PostConstruct
+    public void init() {
+        tempCart = new Cart();
+    }
+
+    public Cart getCurrentCart() {
+        return tempCart;
+    }
+
+    public void add(Long productId) {
+        Product product = productService.findById(productId).orElseThrow(() -> new ResourceNotFoundException("Не удается добавить продукт с id: " + productId + " в корзину. Продукт не найден"));
+        tempCart.add(product);
+    }
+}
+
