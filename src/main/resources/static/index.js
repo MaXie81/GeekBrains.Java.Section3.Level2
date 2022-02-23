ab676f72df615564d37cd00843510a808928da0d
KrakenUser <KrakenUser@yandex.ru>
2022-02-23 11:54:33 +0300

    Web-часть.

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,40 +1,144 @@
-angular.module('app', []).controller('indexController', function ($scope, $http) {
-    $scope.loadProducts = function () {
-        $http.get('http://localhost:8189/webshop/api/v1/products').then(function (response) {
-            $scope.productsList = response.data;
-        });
-    }
+angular
+    .module(
+        'app',
+        ['ngStorage']
+    )
+    .controller(
+        'indexController',
+        function (
+            $scope,
+            $http,
+            $localStorage
+        ) {
+            $scope.tryToAuth =
+                function () {
+                    $http.post('http://localhost:8189/webshop/auth', $scope.user)
+                    .then(
+                        function successCallback(response) {
+                            if (response.data.token) {
+                                $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+                                $localStorage.webShopUser =
+                                    {
+                                        username: $scope.user.username,
+                                        token: response.data.token
+                                    };
 
-    $scope.showProductInfo = function (productId) {
-        $http.get('http://localhost:8189/webshop/api/v1/products/' + productId).then(function (response) {
-            alert(response.data.title);
-        });
-    }
+                                $scope.user.username = null;
+                                $scope.user.password = null;
+                            }
+                        },
+                        function errorCallback(response) {}
+                    );
+                };
 
-    $scope.deleteProductById = function (productId) {
-        $http.delete('http://localhost:8189/webshop/api/v1/products/' + productId).then(function (response) {
+            $scope.tryToLogout =
+                function () {
+                    $scope.clearUser();
+                    $scope.user = null;
+                };
+
+            $scope.clearUser =
+                function () {
+                    delete $localStorage.webShopUser;
+                    $http.defaults.headers.common.Authorization = '';
+                };
+
+            $scope.isUserLoggedIn =
+                function () {
+                    if ($localStorage.webShopUser) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                };
+
+            $scope.authCheck =
+                function () {
+                    $http.get('http://localhost:8189/webshop/auth_check')
+                    .then(
+                        function (response) {
+                            alert(response.data.value);
+                        }
+                    );
+                };
+
+            $scope.loadProducts =
+                function () {
+                    $http.get('http://localhost:8189/webshop/api/v1/products')
+                    .then(
+                        function (response) {
+                            $scope.productsList = response.data;
+                        }
+                    );
+                };
+
+            $scope.showProductInfo =
+                function (productId) {
+                    $http.get('http://localhost:8189/webshop/api/v1/products/' + productId)
+                    .then(
+                        function (response) {
+                            alert(response.data.title);
+                        }
+                    );
+                };
+
+            $scope.removeFromCart =
+                function (productId) {
+                    $http.get('http://localhost:8189/webshop/api/v1/cart/remove/' + productId)
+                    .then(
+                        function (response) {
+                            $scope.loadCart();
+                        }
+                    );
+                };
+
+            $scope.clearCart =
+                function () {
+                    $http.get('http://localhost:8189/webshop/api/v1/cart/clear')
+                    .then(
+                        function (response) {
+                            $scope.loadCart();
+                        }
+                    );
+                };
+
+            $scope.addToCart =
+                function (productId) {
+                    $http.get('http://localhost:8189/webshop/api/v1/cart/add/' + productId)
+                    .then(
+                        function (response) {
+                            $scope.loadCart();
+                        }
+                    );
+                };
+
+            $scope.loadCart =
+                function () {
+                    $http.get('http://localhost:8189/webshop/api/v1/cart')
+                    .then(
+                        function (response) {
+                            $scope.cart = response.data;
+                        }
+                    );
+                };
+
             $scope.loadProducts();
-        });
-    }
-
-    $scope.addToCart = function (productId) {
-        $http.get('http://localhost:8189/webshop/api/v1/cart/add/' + productId).then(function (response) {
             $scope.loadCart();
-        });
-    }
 
-    $scope.loadCart = function () {
-        $http.get('http://localhost:8189/webshop/api/v1/cart').then(function (response) {
-            $scope.cart = response.data;
-        });
-    }
+            if ($localStorage.webShopUser) {
+                try {
+                    let jwt = $localStorage.webShopUser.token;
+                    let payload = JSON.parse(atob(jwt.split('.')[1]));
+                    let currentTime = parseInt(new Date().getTime() / 1000);
+                    if (currentTime > payload.exp) {
+                        console.log("Token is expired!!!");
+                        delete $localStorage.webShopUser;
+                        $http.defaults.headers.common.Authorization = '';
+                    }
+                } catch (e) {
+                }
 
-    $scope.clearCart = function () {
-        $http.get('http://localhost:8189/webshop/api/v1/cart/clear').then(function (response) {
-            $scope.loadCart();
-        });
-    }
-
-    $scope.loadProducts();
-    $scope.loadCart();
-});
+                $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.webShopUser.token;
+            }
+        }
+    );
