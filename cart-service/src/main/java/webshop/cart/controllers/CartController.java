ffca2940e6bfcd1605593c2d4a9ee5074fb64671
KrakenUser <KrakenUser@yandex.ru>
2022-03-03 13:22:30 +0300

    Корзина. Контроллеры. Создан контроллер(CartController).

---
+++ cart-service/src/main/java/webshop/cart/controllers/CartController.java
@@ -0,0 +1,38 @@
+package webshop.cart.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import shop.api.CartDto;
+import webshop.cart.convertes.CartConverter;
+import webshop.cart.services.CartService;
+
+@RestController
+@RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
+public class CartController {
+    private final CartService cartService;
+    private final CartConverter cartConverter;
+
+    @GetMapping("/add/{id}")
+    public void addToCart(@PathVariable Long id) {
+        cartService.add(id);
+    }
+
+    @GetMapping("/clear")
+    public void clearCart() {
+        cartService.clear();
+    }
+
+    @GetMapping("/remove/{id}")
+    public void removeFromCart(@PathVariable Long id) {
+        cartService.remove(id);
+    }
+
+    @GetMapping
+    public CartDto getCurrentCart() {
+        return cartConverter.entityToDto(cartService.getCurrentCart());
+    }
+}
