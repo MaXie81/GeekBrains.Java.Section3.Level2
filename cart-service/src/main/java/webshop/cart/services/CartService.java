bc62498a60a6ee79c7f4202d2fcaef764df0af93
KrakenUser <KrakenUser@yandex.ru>
2022-03-03 13:20:43 +0300

    Корзина. Сервисы. Создан сервис(CartService).

---
+++ cart-service/src/main/java/webshop/cart/services/CartService.java
@@ -0,0 +1,41 @@
+package webshop.cart.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import shop.api.ProductDto;
+import shop.api.ResourceNotFoundException;
+import webshop.cart.integrations.ProductServiceIntegration;
+import webshop.cart.model.Cart;
+
+import javax.annotation.PostConstruct;
+
+@Service
+@RequiredArgsConstructor
+public class CartService {
+    private final ProductServiceIntegration productServiceIntegration;
+    private Cart tempCart;
+
+    @PostConstruct
+    public void init() {
+        tempCart = new Cart();
+    }
+
+    public Cart getCurrentCart() {
+        return tempCart;
+    }
+
+    public void add(Long productId) {
+        ProductDto product = productServiceIntegration
+                .getProductById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Не удается добавить продукт с id: " + productId + " в корзину. Продукт не найден"));
+        tempCart.add(product);
+    }
+
+    public void remove(Long productId) {
+        tempCart.remove(productId);
+    }
+
+    public void clear() {
+        tempCart.clear();
+    }
+}
