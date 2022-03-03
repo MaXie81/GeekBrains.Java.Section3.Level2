2103f5735ecbf93877f98e6adead7484bc93ea2d
KrakenUser <KrakenUser@yandex.ru>
2022-03-03 14:12:58 +0300

    Монолит. БД. Добавлена таблица Категория и поле id связи в таблицу Продукт(V1_init.sql).

--- core-service/src/main/resources/db/migration/V1__init.sql
+++ core-service/src/main/resources/db/migration/V1__init.sql
@@ -1,10 +1,22 @@
-create table products(
-    id bigserial primary key,
-    title varchar(255),
-    price int,
+create table categories(
+    id              bigserial       primary key,
+    title           varchar(255)    unique,
     created_at      timestamp default current_timestamp,
     updated_at      timestamp default current_timestamp
 );
-insert into products(title, price) values('Молоко', 50);
-insert into products(title, price) values('Хлеб', 30);
-insert into products(title, price) values('Сыр', 125);
+
+create table products(
+    id              bigserial       primary key,
+    title           varchar(255),
+    category_id     bigint references categories (id),
+    price           int,
+    created_at      timestamp default current_timestamp,
+    updated_at      timestamp default current_timestamp
+);
+
+insert into categories (title) values ('Хлебные изделия');
+insert into categories (title) values ('Молочные изделия');
+
+insert into products(title, price, category_id) values('Молоко', 50, 2);
+insert into products(title, price, category_id) values('Хлеб', 30, 1);
+insert into products(title, price, category_id) values('Сыр', 125, 2);
