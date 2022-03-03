a613facda5a59f48a2529f08ea47eb4c23cac314
KrakenUser <KrakenUser@yandex.ru>
2022-03-03 12:42:36 +0300

    Корзина. Создан класс запуска приложения модуля(WebshopCartApplication).

---
+++ cart-service/src/main/java/webshop/cart/WebshopCartApplication.java
@@ -0,0 +1,13 @@
+package webshop.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopCartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopCartApplication.class, args);
+    }
+
+}
