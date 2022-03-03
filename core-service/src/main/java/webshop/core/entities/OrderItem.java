ebefd7bc1e3206444023743acf2cba4e1f6ef6ae
KrakenUser <KrakenUser@yandex.ru>
2022-03-03 14:17:35 +0300

    Монолит. Сущности.

--- core-service/src/main/java/webshop/core/entities/OrderItem.java
+++ core-service/src/main/java/webshop/core/entities/OrderItem.java
@@ -13,7 +13,6 @@ import java.time.LocalDateTime;
 @Table(name = "order_items")
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class OrderItem {
     @Id
     @Column(name = "id")
@@ -44,4 +43,12 @@ public class OrderItem {
     @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
+
+    public OrderItem(Product product, Order order, int quantity, int pricePerProduct, int price) {
+        this.product = product;
+        this.order = order;
+        this.quantity = quantity;
+        this.pricePerProduct = pricePerProduct;
+        this.price = price;
+    }
 }
