7486c287a455385ba79697c72012d385727b73e1
KrakenUser <KrakenUser@yandex.ru>
2022-03-03 13:10:45 +0300

    Корзина. Маппинг. Созданы класс-конверторы, преобразующие к dto(CartItemConverter и CartConverter).

---
+++ cart-service/src/main/java/webshop/cart/convertes/CartConverter.java
@@ -0,0 +1,21 @@
+package webshop.cart.convertes;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import shop.api.CartDto;
+import webshop.cart.model.Cart;
+
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class CartConverter {
+    private final CartItemConverter cartItemConverter;
+
+    public CartDto entityToDto(Cart cart) {
+        CartDto cartDto = new CartDto();
+        cartDto.setTotalPrice(cart.getTotalPrice());
+        cartDto.setItems(cart.getItems().stream().map(cartItemConverter::entityToDto).collect(Collectors.toList()));
+        return cartDto;
+    }
+}
---
+++ cart-service/src/main/java/webshop/cart/convertes/CartItemConverter.java
@@ -0,0 +1,18 @@
+package webshop.cart.convertes;
+
+import org.springframework.stereotype.Component;
+import shop.api.CartItemDto;
+import webshop.cart.model.CartItem;
+
+@Component
+public class CartItemConverter {
+    public CartItemDto entityToDto(CartItem cartItem) {
+        CartItemDto cartItemDto = new CartItemDto();
+        cartItemDto.setPrice(cartItem.getPrice());
+        cartItemDto.setPricePerProduct(cartItem.getPricePerProduct());
+        cartItemDto.setQuantity(cartItem.getQuantity());
+        cartItemDto.setProductTitle(cartItem.getProductTitle());
+        cartItemDto.setProductId(cartItem.getProductId());
+        return cartItemDto;
+    }
+}
