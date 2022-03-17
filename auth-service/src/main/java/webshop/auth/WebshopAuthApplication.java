c225373498d122e34f8945683265d022d24dc48e
KrakenUser <KrakenUser@yandex.ru>
2022-03-17 14:41:01 +0300

    Модуль Авторизации.

---
+++ auth-service/src/main/java/webshop/auth/WebshopAuthApplication.java
@@ -0,0 +1,13 @@
+package webshop.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopAuthApplication.class, args);
+    }
+
+}
---
+++ auth-service/src/main/java/webshop/auth/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package webshop.auth.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import webshop.auth.entries.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
