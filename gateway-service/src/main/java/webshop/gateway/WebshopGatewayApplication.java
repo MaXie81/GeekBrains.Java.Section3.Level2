fc044f1e423677b6fdaed1ea815f04d099f4db61
KrakenUser <KrakenUser@yandex.ru>
2022-03-17 14:42:55 +0300

    Модуль GateWay.

---
+++ gateway-service/src/main/java/webshop/gateway/WebshopGatewayApplication.java
@@ -0,0 +1,11 @@
+package webshop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopGatewayApplication.class, args);
+    }
+}
